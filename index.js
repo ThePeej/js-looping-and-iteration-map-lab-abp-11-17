c97df6dfb76c472fa13f24364dfbaea579d415c5
PJ Frias <peejfrias@gmail.com>
2017-11-07 04:22:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  const lowerDrivers = drivers.map(function(name) {return name.toLowerCase()})
+  return lowerDrivers
+}
+
+function nameToAttributes(drivers) {
+  ///for each of array
+  ///find index where space is
+  ///first = array up to but not including
+  ///last= array after space
+  ///newArr.firstName = drivers.map(function(first) {return first})
+  ///newArr.lastName = drivers.map(function(last) {return last})
+
+}
+
+function attributesToPhrase(drivers) {
+  ///for each of array
+  ///name =
+}
