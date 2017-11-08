75a5e86224adfdf82b7bac2fe78e214b8fb8111e
PJ Frias <peejfrias@gmail.com>
2017-11-08 01:04:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,8 +21,10 @@ function nameToAttributes(drivers) {
 */
 
 function attributesToPhrase(drivers) {
-  let match = []
-  for (const name of drivers) {
-
+  let phrase = [];
+  for (const driver of drivers) {
+    let  driverName = drivers.map(function(driver) {return driver.name})
+    console.log(driverName);
+    debugger;
   }
 }
