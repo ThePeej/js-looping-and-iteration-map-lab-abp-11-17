9fffa122944a6327c0a6d5d35fa9c6e456da6981
PJ Frias <peejfrias@gmail.com>
2017-11-08 02:19:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,18 +16,18 @@ function nameToAttributes(drivers) {
 
     for (let x = 0; x < name.length; x++) {
       const letter = name[x]; //letter will iterate through each letter per name of 'drivers' array
-
+      let first = [];
+      let last = [];
       //console.log(letter);
       //debugger;
 
       //checks to see if current letter is a space, then knows proper index of current name element for slicing apart first and last name
       if (letter === " ") {
-        let first = [];
-        let last = [];
+
         first = name.slice(0,x); //first is the letters up to the space
         last = name.slice(x+1); //last is the letters after (and not including) the space
-        sortedNames.firstName = first; //creating object key pairs bro!
-        sortedNames.lastName = last;
+        sortedNames[i].firstName = first; //creating object key pairs bro!
+        sortedNames[i].lastName = last;
         //console.log(first);
         //console.log(last);
         //console.log(sortedNames);
